e5ad067db2a77942bcc99b61093e2ba4387239cf
evankboerchers <evan.boerchers@gmail.com>
2025-04-06 20:39:18 -0700

    project layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,18 +2,3 @@
     
 
 </script>
-<style>
-#game {
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    overflow: hidden;
-    background: #000000;
-}
-</style>
-
-<div class="fixed w-full h-full flex items-center m-auto bg-amber-300">
-    <h1>my website!</h1>
-</div>
