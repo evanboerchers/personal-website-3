abe7d0ac349401736bdfe3be4cc955ce1746072b
evankboerchers <evan.boerchers@gmail.com>
2025-04-09 23:28:12 -0700

    icons

--- src/lib/sections/contact.svelte
+++ src/lib/sections/contact.svelte
@@ -1,10 +1,16 @@
 <script lang="ts">
-
+import {SiGithub} from '@icons-pack/svelte-simple-icons'
+import {BookOpen, GithubIcon} from '@lucide/svelte'
 </script>
 
 <div class="w-full h-screen flex items-center justify-center bg-slate-600">
     <div class='flex flex-col items-center'>
         <h1 class='text-xl'>Contact Me</h1>
+        <p>Reach out by email for inquiries or just to say hi!</p>
+        <a href="mailto:evan.boerchers@gmail.com">evan.boerchers@gmail.com</a>
         <h1 class='text-xl'>My Links</h1>
+        <div class="flex flex-row gap-2">
+            <BookOpen></BookOpen>
+        </div>
     </div>
 </div>
