dcc7bb7ac572f9a5e8b4b5aae5ee42ce930e7a69
evankboerchers <evan.boerchers@gmail.com>
2025-04-10 21:18:25 -0700

    portrait

--- src/lib/sections/about.svelte
+++ src/lib/sections/about.svelte
@@ -1,20 +1,25 @@
 <script lang="ts">
-
+    import portrait from "$lib/assets/portrait.webp"
 </script>
 
-<div class="w-full h-screen flex justify-center items-center bg-white">
-    <div class="">
-        <h1 class="text-xl">
-            What im good at
-        </h1>
-        <p>
-            Words are cool
-        </p>
-        <h1 class="text-xl">
-            What I do for fun
-        </h1>
-        <p>
-            Words are cool
-        </p>
+<section id="about" class="w-full h-screen flex justify-center items-center bg-white">
+    <div class="class flex flex-row">
+        <div>
+            <img class="rounded-full w-[200px]" src={portrait} alt="portrait"/>
+        </div>
+        <div>
+            <h1 class="text-xl">
+                What im good at
+            </h1>
+            <p>
+                Words are cool
+            </p>
+            <h1 class="text-xl">
+                What I do for fun
+            </h1>
+            <p>
+                Words are cool
+            </p>
+        </div>
     </div>
-</div>
+</section>
