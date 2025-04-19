01b367969adec91e010321b2b34271b1bde18ebb
evankboerchers <evan.boerchers@gmail.com>
2025-04-18 22:03:33 -0700

    blog content

---
+++ src/lib/blog/content/001_welcome-to-my-blog.md
@@ -0,0 +1,4 @@
+---
+slug: "welcome-to-my-blog"
+title: "Welcome to my Blog"
+date: "2025-04-10"
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,24 @@
-<script>
+<script lang="ts">
   let { children } = $props();
   import "../app.css";
-  import NavBar from "$lib/navigation/nav-bar.svelte";
+  import NavBar,{ type NavItem }  from "$lib/navigation/nav-bar.svelte";
+
+      
+  const navItems: NavItem[] = [
+        {
+            label: 'About',
+            href: '/about'
+        },
+        {
+            label: 'Projects',
+            href: '/projects'
+        },
+        {
+            label: 'Contact',
+            href: '/contact'
+        },
+    ]
 </script>
-  {@render children()}
+
+<NavBar navItems={navItems}></NavBar>
+{@render children()}
