ef77bc38c078f471cd948a0517ac5ae6c2b5e463
evanboerchers <evan.boerchers@gmail.com>
2025-05-07 16:52:52 -0700

    work

---
+++ src/lib/components/ui/button/button.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import { Button as ButtonPrimitive } from "bits-ui";
+
+    interface Props {
+        children: any
+    }
+    
+    let { children, ...props }: Props = $props()
+    
+</script>
+
+<ButtonPrimitive.Root
+	type="button"
+    {...props}
+>
+	{@render children?.()}
+</ButtonPrimitive.Root>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,21 +1,22 @@
 <script lang='ts'>
 	import Hero from "$lib/sections/hero.svelte";
+	import Button from "$lib/components/ui/button/button.svelte";
 </script>
 
 <div class="mt-10 flex flex-col gap-4">
 	<h1 class="text-5xl">
-		Hi Im Evan. I am Software Engineer, Fitness Enthusiast and Coffee Addict 
+		Hi Im Evan 
 	</h1>
 	<h2 class="text-xl">
-		I am currently working as a Software Engineer in the gaming industry. In my free time I love building stuff on the web.
+		I am Software Engineer, Fitness Enthusiast and Coffee Addict. This is the place where I share what my work and interests.
 	</h2>
+	<Button href='/blog'> My Blog </Button>
 	<div class="flex flex-row gap-6">
 		<div>
-			<div>Check out my blog</div>
-			<div>recent posts</div>
+			<h3>recent posts</h3>
 		</div>
-		<div>
-			<div>More</div>
+	<div>
+			<h3>More</h3>
 			<a href="/about">About me</a>
 			<a href="/projects">Projects</a>
 			<a href="/projects">Contact</a>
