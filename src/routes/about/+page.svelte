f9a81c3f4268df77189c0093768a64de1160a3e9
evankboerchers <evan.boerchers@gmail.com>
2025-06-09 20:11:06 -0700

    about page

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+import portrait from '$lib/assets/portrait.webp'
+</script>
+
+<div class='mx-auto max-w-[500px]'>
+    <h1 class='text-4xl'>Who Am I</h1>
+    <img class='float-right max-w-[200px] m-4 object-cover' src={portrait} alt='me'/>
+    <p>I am Evan Boerchers, a Software developer from Victoria BC.</p>
+</div>
