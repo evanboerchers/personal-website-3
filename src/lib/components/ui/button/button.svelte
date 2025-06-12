65c3d937b5662297803787944fd80b32f1fa4bfa
evankboerchers <evan.boerchers@gmail.com>
2025-06-11 23:13:17 -0700

    format

--- src/lib/components/ui/button/button.svelte
+++ src/lib/components/ui/button/button.svelte
@@ -5,23 +5,28 @@
 
 	type ButtonPrimitiveProps = ComponentProps<typeof ButtonPrimitive.Root>;
 
-	const baseStyle ='inline-flex items-center justify-center px-4 py-2 rounded-md text-sm font-medium transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2 disabled:opacity-50 disabled:pointer-events-none'
+	const baseStyle =
+		'inline-flex items-center justify-center px-4 py-2 rounded-md text-sm font-medium transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2 disabled:opacity-50 disabled:pointer-events-none';
 	const variantClass = {
 		default: 'bg-secondary text-white hover:bg-secondary/90',
-		outline: 'border border-secondary text-secondary hover:bg-secondary/90',
-	} as const
+		outline: 'border border-secondary text-secondary hover:bg-secondary/90'
+	} as const;
 
-	type ButtonVariant = keyof typeof variantClass 
-	
-	interface ButtonProps  {
-		children?: any,
-		variant?: ButtonVariant
+	type ButtonVariant = keyof typeof variantClass;
+
+	interface ButtonProps {
+		children?: any;
+		variant?: ButtonVariant;
 	}
 
-	let { children, class: className, variant = 'default', ...props }: ButtonProps & Omit<ButtonPrimitiveProps, 'class'> = $props();
-
+	let {
+		children,
+		class: className,
+		variant = 'default',
+		...props
+	}: ButtonProps & Omit<ButtonPrimitiveProps, 'class'> = $props();
 </script>
 
-<ButtonPrimitive.Root class={clsx(baseStyle, variantClass[variant],  className)} {...props}>
+<ButtonPrimitive.Root class={clsx(baseStyle, variantClass[variant], className)} {...props}>
 	{@render children?.()}
 </ButtonPrimitive.Root>
--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,20 +1,21 @@
 <script lang="ts">
-	import Button from "$lib/components/ui/button/button.svelte";
-	import Link from "$lib/components/ui/button/link.svelte";
-
+	import Button from '$lib/components/ui/button/button.svelte';
+	import Link from '$lib/components/ui/button/link.svelte';
 </script>
 
-<div class= 'max-w-[500px] mx-auto'>
-	<h1 class='text-4xl'>Get in touch</h1>
-	<div class='flex flex-col gap-4'>
-		<section class='mt-4'>
-			<p class='text-lg'>Happy to chat! Feel free to drop me an email </p>
-			<Button class="block mt-2" href="mailto:evan.boerchers@gmail.com">evan.boerchers@gmail.com</Button>
-			<p class='text-text-secondary text-sm'>No solicitation please</p>
+<div class="max-w-[500px] mx-auto">
+	<h1 class="text-4xl">Get in touch</h1>
+	<div class="flex flex-col gap-4">
+		<section class="mt-4">
+			<p class="text-lg">Happy to chat! Feel free to drop me an email</p>
+			<Button class="block mt-2" href="mailto:evan.boerchers@gmail.com"
+				>evan.boerchers@gmail.com</Button
+			>
+			<p class="text-text-secondary text-sm">No solicitation please</p>
 		</section>
 		<div>
-			<h2 class='font-bold text-sm'>My Links</h2>
-			<ul class='list-disc marker:text-accent pl-4'>
+			<h2 class="font-bold text-sm">My Links</h2>
+			<ul class="list-disc marker:text-accent pl-4">
 				<li>
 					<Link>LinkedIn</Link>
 				</li>
