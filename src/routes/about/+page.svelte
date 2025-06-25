03843bfe3e956c0fafbc632a00f32ba9b2e5931b
evanboerchers <evan.boerchers@gmail.com>
2025-06-25 16:46:43 -0700

    round img

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -5,9 +5,9 @@
 <article class="prose dark:prose-invert mx-auto max-w-[500px]">
 	<h1 class="text-4xl">Who Am I</h1>
 	<img
-		class="float-right m-4 max-w-[200px] object-cover"
+		class="float-right m-4 max-w-[200px] object-cover rounded-full"
 		src={portrait}
 		alt="me"
 	/>
-	<p>I am Evan Boerchers, a Software developer from Victoria BC.</p>
+	<p>My name is Evan Boerchers. I am a Software Engineer from Victoria, British Colombia. Currently I am working at Blastworks, a .</p>
 </article>
