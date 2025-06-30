b4929c04e8d484bdf55e9ddb8bddff88624f1fdb
evankboerchers <evan.boerchers@gmail.com>
2025-06-30 00:38:18 -0700

    moved about to .svx

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,13 +1,7 @@
 <script lang="ts">
-	import portrait from '$assets/portrait.webp';
+	import AboutMe from '$content/about/about.svx'
 </script>
 
 <article class="prose dark:prose-invert mx-auto max-w-[500px]">
-	<h1 class="text-4xl">Who Am I</h1>
-	<img
-		class="float-right m-4 max-w-[200px] object-cover rounded-full"
-		src={portrait}
-		alt="me"
-	/>
-	<p>My name is Evan Boerchers. I am a Software Engineer from Victoria, British Colombia. Currently I am working at Blastworks, a .</p>
+	<AboutMe/>
 </article>
