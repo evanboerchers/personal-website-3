4fce0ffe3b9ba48c3512081804f91bb2ba6ddfa3
evanboerchers <evan.boerchers@gmail.com>
2025-07-03 17:59:30 -0700

    added cn utility for class merges

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,7 +1,14 @@
+import { type ClassValue, clsx } from 'clsx';
+import { twMerge } from 'tailwind-merge';
+
 export const formatDate = (date: string): string => {
 	return new Intl.DateTimeFormat('en-GB', {
 		day: 'numeric',
 		month: 'short',
 		year: 'numeric'
 	}).format(new Date(date));
+};
+
+export const cn = (...inputs: ClassValue[]): string => {
+	return twMerge(clsx(inputs));
 };
