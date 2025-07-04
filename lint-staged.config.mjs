dd7b9c6663ebd8e50372821ffaeed12bea46b240
evanboerchers <evan.boerchers@gmail.com>
2025-07-04 11:49:48 -0700

    added svelte files to lint-staged

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -3,6 +3,10 @@
  * @type {import('lint-staged').Configuration}
  */
 export default {
-	'*.{js,jsx,cjs,mjs,ts,tsx,json,css,scss,md,html}': ['prettier --write'],
-	'*.{js,jsx,ts,tsx,cjs,mjs}': ['eslint --no-warn-ignored --max-warnings 0']
+	'*.{js,jsx,cjs,mjs,ts,tsx,json,css,scss,md,html,svelte}': [
+		'prettier --write'
+	],
+	'*.{js,jsx,ts,tsx,cjs,mjs,svelte}': [
+		'eslint --no-warn-ignored --max-warnings 0'
+	]
 };
