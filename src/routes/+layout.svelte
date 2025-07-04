e7ee11ed24a3399ef772d055ebcb85f39f4e26b1
evanboerchers <evan.boerchers@gmail.com>
2025-07-04 11:47:46 -0700

    added page transition

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,11 @@
 <script lang="ts">
-	let { children } = $props();
 	import '../app.css';
 	import NavBar from '$lib/components/navigation/nav-bar.svelte';
+	import { fly } from 'svelte/transition';
+	import { quintInOut } from 'svelte/easing';
 	import { ModeWatcher } from 'mode-watcher';
 	import Footer from '$lib/components/footer.svelte';
+	let { children, data } = $props();
 </script>
 
 <div id="app" class="bg-primary min-h-screen grid grid-cols-[100%] grid-rows-[auto_1fr_auto]">
@@ -11,8 +13,12 @@
 	<header>
 		<NavBar></NavBar>
 	</header>
-	<div id="layout" class="mt-8 mr-16 mb-8 ml-16">
-		{@render children()}
-	</div>
-	<Footer />
+	{#key data.pathname}
+		<div id='transition' in:fly={{easing: quintInOut, y: 10, duration: 300, delay: 350 }} out:fly={{easing: quintInOut,y: 10, duration: 300}}>
+			<div id="layout" class="mt-8 mr-16 mb-8 ml-16">
+				{@render children?.()}
+			</div>
+			<Footer />
+		</div>
+	{/key}
 </div>
--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,1 +1,10 @@
+import type { PageLoad } from './$types';
+
 export const prerender = true;
+
+export const load: PageLoad = async ({ url }) => {
+	const { pathname } = url;
+	return {
+		pathname
+	};
+};
