9efc1c0cb84d2653a5cb65a75e30ee9bb2bb083b
evanboerchers <evan.boerchers@gmail.com>
2025-07-07 22:19:55 -0600

    fixed blog layout at low res

--- src/routes/blog/+layout.svelte
+++ src/routes/blog/+layout.svelte
@@ -5,7 +5,7 @@
 </script>
 
 <div
-	class="my-20 grid grid-cols-[var(--sidebar-width)_1fr_var(--sidebar-width)] grid-rows-[100%] gap-4 md:grid-cols-[var(--sidebar-width)_1fr]"
+	class="my-20 grid grid-cols-[var(--sidebar-width)_1fr] grid-rows-[100%] gap-4 md:grid-cols-[var(--sidebar-width)_1fr_var(--sidebar-width)]"
 >
 	<div class="">
 		<BlogSidebar tags={data.tags} posts={data.posts}></BlogSidebar>
