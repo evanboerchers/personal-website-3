5a6f570fc8f58084f4cdfe93241241e10f940459
evanboerchers <evan.boerchers@gmail.com>
2025-07-10 20:35:25 -0600

    added nav bar indicator and transitions

--- src/lib/components/navigation/nav-bar.svelte
+++ src/lib/components/navigation/nav-bar.svelte
@@ -3,21 +3,41 @@
 	import { Menubar } from 'bits-ui';
 	import SiteIcon from './site-icon.svelte';
 	import { navItems } from '$lib/navigation';
-	let isMobileMenuOpen = false;
+	import { page } from '$app/state';
+	import { cn } from '$lib/utils';
+	import { fade as itemTransition } from 'svelte/transition';
+	import { circOut, circIn } from 'svelte/easing';
+	const isCurrent = (href: string) => {
+		return href === page.url.pathname;
+	};
 </script>
 
 {#snippet item(href: string, label: string)}
 	<Menubar.Menu>
-		<Menubar.Trigger class="text-bold hover:text-accent text-lg">
-			<a {href}>{label}</a>
+		<Menubar.Trigger>
+			<a {href} class="relative block text-lg">
+				<span
+					class={cn(
+						{ 'font-bold': isCurrent(href) },
+						{ 'hover:text-accent': !isCurrent(href) }
+					)}
+				>
+					{label}
+					{#if isCurrent(href)}
+						<span
+							class="bg-accent absolute -bottom-0.5 left-0 h-0.5 w-[100%]"
+							in:itemTransition={{ easing: circOut, delay: 0, duration: 400 }}
+							out:itemTransition={{ easing: circIn, delay: 0, duration: 400 }}
+						>
+						</span>
+					{/if}
+				</span>
+			</a>
 		</Menubar.Trigger>
 	</Menubar.Menu>
 {/snippet}
 
-<nav
-	class="top-0 left-0 flex w-full items-center justify-between px-4 py-3 shadow-sm"
->
-	<!-- Logo -->
+<nav class="top-0 left-0 flex w-full items-center justify-between px-4 py-3">
 	<Menubar.Root class="w-full">
 		<!-- TODO: Change to some max content width -->
 		<div class="mx-auto flex max-w-[200rem] justify-between px-15 py-3">
@@ -26,7 +46,7 @@
 			</a>
 			<div class="flex gap-4">
 				<ModeToggle />
-				{#each navItems as navItem}
+				{#each navItems as navItem (navItem.label)}
 					{@render item(navItem.href, navItem.label)}
 				{/each}
 			</div>
