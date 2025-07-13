190eb882a517e18549ce6ea8fcbafc5d089141e4
evanboerchers <evan.boerchers@gmail.com>
2025-07-13 14:39:08 -0600

    format

--- src/routes/blog/+layout.svelte
+++ src/routes/blog/+layout.svelte
@@ -7,10 +7,10 @@
 <div
 	class="my-20 grid grid-cols-[var(--sidebar-width)_1fr] grid-rows-[100%] gap-4 md:grid-cols-[var(--sidebar-width)_1fr_var(--sidebar-width)]"
 >
-	<div class="">
+	<div>
 		<BlogSidebar tags={data.tags} posts={data.posts}></BlogSidebar>
 	</div>
-	<div class="w-full">
+	<div class="mx-10 w-full">
 		{@render children()}
 	</div>
 	<div></div>
