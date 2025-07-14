390c70a41ad32a155259558fbfcbfc2275c4197d
evanboerchers <evan.boerchers@gmail.com>
2025-07-14 01:08:10 -0600

    made blog sidebar sticky

--- src/lib/components/blog/blog-sidebar.svelte
+++ src/lib/components/blog/blog-sidebar.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import type { BlogTagEnum } from '$content/blog/types';
+	import { cn } from '$lib/utils';
 	import Link from '../ui/link.svelte';
 	import List from '../ui/list.svelte';
 	import BlogTags from './blog-tags.svelte';
@@ -13,12 +14,13 @@
 			title: string;
 			slug: string;
 		}[];
+		class?: string;
 	}
 
-	let { tags, posts }: BlogSidebarProps = $props();
+	let { tags, posts, class: className }: BlogSidebarProps = $props();
 </script>
 
-<aside>
+<aside class={cn('p-4', className)}>
 	<input
 		class="border-border mb-6 border-1 px-4 py-1"
 		placeholder="Search titles..."
@@ -28,7 +30,7 @@
 			Recent Posts
 		</h2>
 		<List class="pl-4">
-			{#each posts as post}
+			{#each posts as post (post.slug)}
 				<li>
 					<Link href={`/blog/${post.slug}`}>{post.title}</Link>
 				</li>
@@ -45,13 +47,13 @@
 		</h2>
 		<List class="pl-4">
 			<li>
-				<Link href={'/blog'}>All Posts</Link>
+				<Link href="/blog">All Posts</Link>
 			</li>
 			<li>
-				<Link href={'/'}>Home</Link>
+				<Link href="/">Home</Link>
 			</li>
 			<li>
-				<Link href={'/about'}>About Me</Link>
+				<Link href="/about">About Me</Link>
 			</li>
 		</List>
 	</div>
--- src/routes/blog/+layout.svelte
+++ src/routes/blog/+layout.svelte
@@ -7,9 +7,11 @@
 <div
 	class="my-20 grid grid-cols-[var(--sidebar-width)_1fr] grid-rows-[100%] gap-4 md:grid-cols-[var(--sidebar-width)_1fr_var(--sidebar-width)]"
 >
-	<div>
-		<BlogSidebar tags={data.tags} posts={data.posts}></BlogSidebar>
-	</div>
+	<BlogSidebar
+		class="sticky top-20 self-start"
+		tags={data.tags}
+		posts={data.posts}
+	></BlogSidebar>
 	<div class="mx-10 w-full">
 		{@render children()}
 	</div>
