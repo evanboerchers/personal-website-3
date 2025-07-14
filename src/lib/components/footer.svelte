edd14fabfba8ca19e8eba10d3010ec34d44a5a9b
evanboerchers <evan.boerchers@gmail.com>
2025-07-13 23:43:48 -0600

    footer improvements

--- src/lib/components/footer.svelte
+++ src/lib/components/footer.svelte
@@ -7,29 +7,39 @@
 
 <footer class="bg-secondary">
 	<div class="mx-auto max-w-[var(--content-max-width)]">
-		<div class="flex w-full flex-row gap-20 p-20">
-			<div>
-				<h3 class="font-bold">Pages</h3>
-				<List class="pl-5">
-					{#each navItems as item (item.label)}
-						<li>
-							<Link href={item.href}>{item.label}</Link>
-						</li>
-					{/each}
-				</List>
+		<div class="flex flex-row justify-between p-20">
+			<div class="flex flex-row gap-20">
+				<div>
+					<h3 class="font-bold">Pages</h3>
+					<List class="pl-5">
+						{#each navItems as item (item.label)}
+							<li>
+								<Link href={item.href}>{item.label}</Link>
+							</li>
+						{/each}
+					</List>
+				</div>
+				<div>
+					<h3 class="font-bold">Links</h3>
+					<List class="marker:text-accent list-disc pl-5">
+						{#each myLinks as item (item.label)}
+							<li>
+								<Link href={item.href}>{item.label}</Link>
+							</li>
+						{/each}
+					</List>
+				</div>
 			</div>
-			<div>
-				<h3 class="font-bold">Links</h3>
-				<List class="marker:text-accent list-disc pl-5">
-					{#each myLinks as item (item.label)}
-						<li>
-							<Link href={item.href}>{item.label}</Link>
-						</li>
-					{/each}
-				</List>
-			</div>
-			<div>
-				<p>Evan Boerchers @ 2025</p>
+			<div class="max-w-[250px] text-sm">
+				<p class="italic">©2025 Evan Boerchers</p>
+				<p class="mt-2">
+					Built using <Link href="https://svelte.dev/">SvelteKit</Link> and hosted
+					with <Link href="https://www.cloudflare.com/">Cloudflare</Link>.
+					Source code available on <Link
+						href="https://github.com/evanboerchers/personal-website-3"
+						>Github</Link
+					>
+				</p>
 			</div>
 		</div>
 	</div>
