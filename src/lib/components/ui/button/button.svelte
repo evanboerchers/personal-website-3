5d73f7cbdbc1efd98ffb2ee7c13011d054fdc8f0
evankboerchers <evan.boerchers@gmail.com>
2025-07-19 22:18:15 -0700

    added sidebar transitions

--- src/lib/components/ui/button/button.svelte
+++ src/lib/components/ui/button/button.svelte
@@ -6,7 +6,7 @@
 	type ButtonPrimitiveProps = ComponentProps<typeof ButtonPrimitive.Root>;
 
 	const baseStyle =
-		'inline-flex items-center justify-center px-4 py-2 rounded-md text-sm font-medium transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2 disabled:opacity-50 disabled:pointer-events-none';
+		'inline-flex cursor-pointer items-center justify-center px-4 py-2 rounded-md text-sm font-medium transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2 disabled:opacity-50 disabled:pointer-events-none';
 	const variantClass = {
 		default: 'bg-primary text-on-primary hover:bg-primary/90',
 		outline:
--- src/routes/blog/+layout.svelte
+++ src/routes/blog/+layout.svelte
@@ -3,33 +3,43 @@
 	import Button from '$lib/components/ui/button/button.svelte';
 	import { cn } from '$lib/utils';
 	import { ChevronLeft, ChevronRight } from '@lucide/svelte';
+	import { fly } from 'svelte/transition';
+	import { quintInOut } from 'svelte/easing';
 
 	let { children, data } = $props();
-
-	let sidebarOpen = $state(false);
+	let sidebarOpen = $state(true);
 </script>
 
 <div
 	class={cn(
-		'my-20 grid grid-rows-[100%]',
+		'my-20 grid grid-rows-[100%] transition-[grid-template-columns] duration-300',
 		sidebarOpen
-			? 'grid-cols-[var(--sidebar-width)_1fr] lg:grid-cols-[var(--sidebar-width)_1fr_var(--sidebar-width)]'
-			: 'grid-cols-[auto_1fr] lg:grid-cols-[20px_1fr_20px]'
+			? 'grid-cols-[250px_1fr] lg:grid-cols-[250px_1fr_250px]'
+			: 'grid-cols-[40px_1fr] lg:grid-cols-[40px_1fr_40px]'
 	)}
 >
 	<div class="sticky top-20 flex flex-col items-end self-start">
-		<Button class="mb-4" onclick={() => (sidebarOpen = !sidebarOpen)}>
+		<Button
+			class="mb-4 rounded-full p-2"
+			variant="outline"
+			onclick={() => (sidebarOpen = !sidebarOpen)}
+		>
 			{#if sidebarOpen}
-				<ChevronLeft />
+				<ChevronLeft class="h-7 w-7" />
 			{:else}
-				<ChevronRight />
+				<ChevronRight class="h-7 w-7" />
 			{/if}
 		</Button>
+
 		{#if sidebarOpen}
-			<BlogSidebar tags={data.tags} posts={data.posts}></BlogSidebar>
+			<div
+				in:fly={{ easing: quintInOut, x: 10, duration: 300 }}
+				out:fly={{ easing: quintInOut, x: 10, duration: 300 }}
+			>
+				<BlogSidebar tags={data.tags} posts={data.posts} />
+			</div>
 		{/if}
 	</div>
-
 	<div class="w-full">
 		{@render children()}
 	</div>
