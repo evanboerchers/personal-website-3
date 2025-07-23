762019aea79e152d114a6b9cf88293196d2df821
evanboerchers <evan.boerchers@gmail.com>
2025-07-22 17:04:40 -0700

    basic search algo

--- src/lib/components/blog/blog-search.svelte
+++ src/lib/components/blog/blog-search.svelte
@@ -1,21 +1,46 @@
 <script lang="ts">
-	// import type { BlogSearchEntry } from "$content/blog/types";
+	import { Search } from '@lucide/svelte';
 
-	// interface BlogSearchProps {
-	// 	searchEntries: BlogSearchEntry[]
-	// }
+	import type { BlogSearchEntry } from '$content/blog/types';
+	import Fuse from 'fuse.js';
 
-	// let { searchEntries }: BlogSearchProps = $props()
+	interface BlogSearchProps {
+		searchEntries: BlogSearchEntry[];
+	}
 
-	// let searchString = $state()
+	let { searchEntries }: BlogSearchProps = $props();
+
+	const searchCollection = new Fuse(searchEntries, {
+		keys: ['slug', 'title', 'description', 'tags'],
+		includeScore: true,
+		threshold: 0.3
+	});
+
+	let searchString = $state('');
+
+	const searchPosts = (query: string): BlogSearchEntry[] => {
+		const results = searchCollection.search(query);
+		results.sort((a, b) => (a.score ?? 0) - (b.score ?? 0));
+		return results.map((result) => result.item);
+	};
+
+	let foundEntries = $derived(searchPosts(searchString));
 </script>
 
 <div>
-	<div>
+	<div class="relative">
+		<Search class="text-border absolute top-2.25 left-2.25 h-4 w-4" />
 		<input
-			class="border-border mb-6 border-1 px-4 py-1"
-			placeholder="Search titles..."
+			class="border-border mb-6 border-1 px-8 py-1.5 text-sm"
+			placeholder="Search posts..."
+			bind:value={searchString}
 		/>
 	</div>
-	<ul></ul>
+	<ul>
+		{#each foundEntries as entry (entry.slug)}
+			<li>
+				{entry.slug}
+			</li>
+		{/each}
+	</ul>
 </div>
