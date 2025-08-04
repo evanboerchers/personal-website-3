f01879476ed90f2b8e405d824c9a55888d43d760
evankboerchers <evan.boerchers@gmail.com>
2025-08-03 21:25:33 -0700

    blog placeholder

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,9 +1,27 @@
 <script lang="ts">
 	import CoffeeChess from '$content/project/coffee-chess.svx';
+	import PersonalBlog from '$content/project/personal-blog.svx';
+	import Link from '$lib/components/ui/link.svelte';
+	import List from '$lib/components/ui/list.svelte';
 	import PageHeading from '$lib/components/ui/page-heading.svelte';
+	import Prose from '$lib/components/ui/typography/prose.svelte';
 </script>
 
 <div class="mx-auto max-w-[var(--article-max-width)]">
-	<PageHeading class="text-4xl">My Stuff</PageHeading>
-	<CoffeeChess />
+	<section class="border-primary-variant border-b pb-8">
+		<PageHeading class="text-4xl">My Stuff</PageHeading>
+		<CoffeeChess />
+		<PersonalBlog />
+	</section>
+	<section class="mt-12">
+		<Prose>
+			<h1>Other Stuff</h1>
+			<List>
+				<li>
+					<Link>Website</Link> showing how sorting algorithms work in real time.
+					Built with Reactjs source on <Link href="">Github</Link>
+				</li>
+			</List>
+		</Prose>
+	</section>
 </div>
