6aa7de821237f3524b6788f58599a003feb72674
evankboerchers <evan.boerchers@gmail.com>
2025-08-03 21:00:18 -0700

    got blog column variant working based on element width

--- src/lib/breakpoints.ts
+++ src/lib/breakpoints.ts
@@ -1,54 +1,55 @@
-// import { debounce } from "./utils";
+export const breakpoints = {
+	xs: '--breakpoints-xs',
+	sm: '--breakpoints-sm',
+	md: '--breakpoints-md',
+	lg: '--breakpoints-lg',
+	xl: '--breakpoints-xl',
+	'2xl': '--breakpoints-2xl'
+} as const;
 
-// export const breakpoints = {
-// 	sm: '--breakpoints-sm',
-// 	md: '--breakpoints-md',
-// 	lg: '--breakpoints-lg',
-// 	xl: '--breakpoints-xl',
-// 	'2xl': '--breakpoints-2xl'
-// } as const;
+export type Breakpoint = keyof typeof breakpoints;
 
-// export type Breakpoint = keyof typeof breakpoints;
+let pixelBreakpoints: Record<Breakpoint, number> = {
+	xs: 475,
+	sm: 640,
+	md: 768,
+	lg: 1024,
+	xl: 1280,
+	'2xl': 1536
+};
 
-// let _initialized = false
-// let _breakpoint = $state<Breakpoint>('sm')
+let _initialized = false;
+let breakpoint = $state<Breakpoint>('sm');
 
-// function setup() {
-//     window.addEventListener('resize', debounce(updateBreakpoint, 200))
-// }
+const updateBreakpoint = () => {
+	breakpoint = getBreakpoint(window.innerWidth);
+};
 
-// export function useeBreakpoint() {
+const getBreakpoint = (width: number) => {
+	let current: Breakpoint = 'sm';
+	for (const bp of Object.entries(pixelBreakpoints)) {
+		if (width >= bp[1]) current = bp[0] as Breakpoint;
+	}
+	return current;
+};
 
-// }
+export function useeBreakpoint() {
+	const styles = getComputedStyle(document.documentElement);
 
-// const styles = getComputedStyle(document.documentElement);
-// console.log('here ---' + parseInt(styles.getPropertyValue(breakpoints.sm)))
+	if (!_initialized) {
+		pixelBreakpoints = {
+			...pixelBreakpoints,
+			sm: parseInt(styles.getPropertyValue(breakpoints.sm)),
+			md: parseInt(styles.getPropertyValue(breakpoints.md)),
+			lg: parseInt(styles.getPropertyValue(breakpoints.lg)),
+			xl: parseInt(styles.getPropertyValue(breakpoints.xl)),
+			'2xl': parseInt(styles.getPropertyValue(breakpoints['2xl']))
+		};
+		window.addEventListener('resize', () => {
+			updateBreakpoint();
+		});
+		_initialized = true;
+	}
 
-// const updateBreakpoint = () => {
-// 	// _breakpoint = getBreakpoint(window.innerWidth)
-// }
-
-// const getBreakpoint = () => {
-
-// }
-
-// export function getCurrentBreakpoint(): Breakpoint {
-// 	if (typeof window === 'undefined') return 'sm';
-
-// 	const pixelBreakpoints: Record<Breakpoint, number> = {
-// 		sm: parseInt(styles.getPropertyValue(breakpoints.sm)) || 640,
-// 		md: parseInt(styles.getPropertyValue(breakpoints.md)) || 768,
-// 		lg: parseInt(styles.getPropertyValue(breakpoints.lg)) || 1024,
-// 		xl: parseInt(styles.getPropertyValue(breakpoints.xl)) || 1280,
-// 		'2xl': parseInt(styles.getPropertyValue(breakpoints['2xl'])) || 1536
-// 	};
-
-// 	const width = window.innerWidth;
-// 	let current: Breakpoint = 'sm';
-
-// 	for (const bp of Object.entries(pixelBreakpoints)) {
-// 		if (width >= bp[1]) current = bp[0] as Breakpoint;
-// 	}
-
-// 	return current;
-// }
+	return breakpoint;
+}
--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -1,15 +1,38 @@
 <script lang="ts">
 	import BlogList from '$lib/components/blog/blog-list.svelte';
 	import type { BlogPreviewVariant } from '$lib/components/blog/blog-preview.svelte';
+	import { onMount } from 'svelte';
 	const { data } = $props();
 
+	let variant = $state<BlogPreviewVariant>('stacked');
+
 	function getVariant(width: number): BlogPreviewVariant {
-		return width > 400 ? 'default' : 'stacked';
+		return width > 475 ? 'default' : 'stacked';
 	}
 
-	let variant = $derived(getVariant(200));
+	function updateVariant(width: number) {
+		variant = getVariant(width);
+	}
+
+	let width = 0;
+
+	let blogContainerEl: HTMLElement | null = null;
+
+	onMount(() => {
+		if (!blogContainerEl) return;
+
+		const observer = new ResizeObserver(([entry]) => {
+			const newWidth = entry.contentRect.width;
+			if (newWidth !== width) {
+				width = newWidth;
+				updateVariant(width);
+			}
+		});
+		observer.observe(blogContainerEl);
+		return () => observer.disconnect();
+	});
 </script>
 
-<div>
+<div bind:this={blogContainerEl}>
 	<BlogList {variant} tags={data.tags} posts={data.posts} />
 </div>
