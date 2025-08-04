31e04813ffdb294f32c033a4ac8e35c14b990185
evankboerchers <evan.boerchers@gmail.com>
2025-08-03 17:13:18 -0700

    mocked out some work

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -1,6 +1,15 @@
 <script lang="ts">
 	import BlogList from '$lib/components/blog/blog-list.svelte';
+	import type { BlogPreviewVariant } from '$lib/components/blog/blog-preview.svelte';
 	const { data } = $props();
+
+	function getVariant(width: number): BlogPreviewVariant {
+		return width > 400 ? 'default' : 'stacked';
+	}
+
+	let variant = $derived(getVariant(200));
 </script>
 
-<BlogList variant="default" tags={data.tags} posts={data.posts} />
+<div>
+	<BlogList {variant} tags={data.tags} posts={data.posts} />
+</div>
